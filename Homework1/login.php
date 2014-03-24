33f59740562542251db07bdeb73f0abb0a28f421
Sindrey <sindrey91@gmail.com>
2014-03-24 21:25:30 +0200

    Fixed array search and Session ADD
    Session added -> Can't logout : -/
    Search in array fixed ;
    Crashed : no msg on empty fields.
    Logic fixing in progress.

--- Homework1/login.php
+++ Homework1/login.php
@@ -1,88 +1,77 @@
 <?php 
 define ( 'SITE_ENABLED' , true);
-define ( 'SITE_LOGGED_IN' , false);
-
+$site_logged_in = false;
 if (SITE_ENABLED == true) {
 
 $accountArray = array("sayran" => '27121198',"sindrey" => 'foo' , "bar" => 'foobar');
 		
-	if (isset($_POST['send']) && (($user_login != '')  && ($user_password!='')))
+	if (isset($_POST['send']))
 	{	
 		$user_login = $_POST['login'];
 		$user_password = $_POST['password'];
-		echo $user_password .'<br>';
-		$user_encrypted_password = md5($user_password);
-		echo $user_encrypted_password .'<br>';
-		echo $user_login .'<br>';
-		echo md5($accountArray[$user_login]);
-		if(in_array($user_login, $accountArray, true))
-			{
-				echo '$accountArray[$user_login]';
-				if ($user_encrypted_password == md5($accountArray[$user_login]))
-				{
-					echo 'Hello ' . htmlspecialchars($_POST["login"]) . '!' . '<br>';
-					echo $user_encrypted_password;
-				}
-			}
-		else
+
+		if (($user_login != '')  && ($user_password!=''))
 		{
-			echo "Hmm stange thing... It seams you have been mistaken..";
+			$user_encrypted_password = md5($user_password);
+
+			if(array_key_exists($user_login, $accountArray))
+				{
+					if ($user_encrypted_password === md5($accountArray[$user_login]))
+					{
+						echo '<br>' . 'Hello ' . htmlspecialchars($_POST["login"]) . '!' . '<br>';
+						session_start();
+						$_SESSION['username'] = $user_login;
+						$site_logged_in = true;
+					}
+					else 
+					{
+						echo '<br>' . "Hmm... password is incorrect.. try one more time : )" . '<br>';
+					}
+				}
+			else
+			{
+				echo "Hmm stange thing... It seams you have been mistaken..";
+			}
 		}
-		//echo 'Hello ' . htmlspecialchars($_POST["login"]) . '!' . '<br>';
 	}
-	else 
+	else if (isset($_POST['send']) && (($user_login != '')  || ($user_password!='')))
 	{
 		echo " Login and Password fields must NOT be empty !";
 	}
-	/*$msg = " 27121198 "; 
-    $encrypted_text = md5 ($msg); 
-    echo("<b>Plain Text : </b>"); 
-    echo($msg); 
-    echo("<p><b>Encrypted Text : </b>"); 
-    echo($encrypted_text);	
-*/
+}
+
+elseif ($site_logged_in = true) {
+	session_start();
+	if (isset($_POST['logout']))
+	{
+		unset($_SESSION['username']);
+		session_destroy();
+		$site_logged_in = false;
+	}
+	else
+	{
+		echo '<br>' . 'Hello ' . htmlspecialchars($_POST["login"]) . '!' . '<br>';
+	}
 }
 ?>
+
 <html>
 	<head>
 	</head>
 	<body>
-		<?php
-		 if (SITE_ENABLED == true) : ?>	
-		<form method = "post" action = "">
+		<?php if ($site_logged_in == true): ?>
+			<input type = "submit" value = "Log-out" name = 'logout'>
+			<?php endif; ?>
+		<?php if (SITE_ENABLED == true && $site_logged_in == false) : ?>	
+			<form method = "post" action = "">
 			Login :<br>
 			<input type = "text" name = "login"/><br>
 			Password: <br>
 			<input type = "password" size = 10 maxlen = 16 name = "password"/><br>
 			<input type = "submit" value = "Sign-in" name = "send">
-		<?php else :?>
+		<?php elseif (SITE_ENABLED == false):?>
 			Sorry, site is curently offline. Please be patient.
 		<?php endif; ?>
-		<?php if (SITE_LOGGED_IN == true): ?>
-		<input type = "submit" value = "Log-out">
-		<?php endif; ?>
+		
 	</body>
 </html>
-
-<!-- <html>
-
-	<head>
-		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
-		<script language = "javascript" ></script>
-		<title>PHP TSI</title>
-	</head>
-	<body>
-		<form method = "post" action = http://localhost/Homework1/login.php>
-			Login :<br>
-			<input type = "text" name = "login"/><br>
-			Password: <br>
-			<input type = "password" size = 8 maxlen = 16 name = "password"/><br>
-			<input type = "submit" value = "Sign-in" name = "send">
-			<input type = "submit" value = "Reset">
-			<tr valign="top">
-      			 <td align="right">Sorry, site is curently ofline. PLease be patient.</td>
-   			</tr>
-		</div>
-		<div id="footer"></div>
-	</body>
-</html> -->

3038aa3f9a63719f95f366c71e33e01f4a412c2e
Sindrey <sindrey91@gmail.com>
2014-03-26 20:55:18 +0200

    Part 1
    Made news array and added correct printing.
    Still need to do:
    1. wake-up
    2. add time array
    3. add random time from time array to printing data.
    4. (?fix it ?)

--- Homework2/ixdex.php
+++ Homework2/ixdex.php
@@ -1,31 +1,33 @@
 <?php 
-$second = true;
-/*	$fisrt_number = $_POST['startNumber'];
-	$second_number = $_POST['endNumber'];
-	$added_value = $_POST['addNumber'];
-	$result;
 
-	if(is_numeric($fisrt_number) && is_numeric($second_number) && is_numeric($added_value))
-	{
-		for($fisrt_number; $fisrt_number <= $second_number ; $fisrt_number ++)
-		{
-			$result = $fisrt_number + $added_value;
-			echo $fisrt_number . " added by " . $added_value . " = " . $result . '<br>';
-		}
-	}
-	else
-	{
-		echo " Sorry, some value is not number !";
-	}*/
+$news_array  = array('news' => 
+						array('isActive' => true ,
+							  // 'news_date' => date(),
+							   'news_tag' => "Hello, fella",
+							   'news_content' => "Some content" ),
+					 'next_news' => 
+					 	array('isActive' => false ,
+							 //  'news_date' => date(),
+							   'news_tag' => "Hello, fella",
+							   'news_content' => "Some content" ),
+					 'next_news1' => 
+					 	array('isActive' => true ,
+							 //  'news_date' => date(),
+							   'news_tag' => "Brooogue kick",
+							   'news_content' => "Some content" ),
+					 'next_news2' => 
+					 	array('isActive' => false ,
+							 //  'news_date' => date(),
+							   'news_tag' => "Hello, fella",
+							   'news_content' => "Some content" ),
+					 'next_news3' => 
+					 	array('isActive' => true ,
+							 //  'news_date' => date(),
+							   'news_tag' => "Irish curse Back-Breaker",
+							   'news_content' => "Some content" ));
+var_dump($news_array);
+						 
 
-$users = array("users" => 
-			 array("Andrew " =>' Sileckij' ,
-			 "sindrey" => 'foo', 
-			 "Artur" => 'Petjukevich')
-		 	  );
-
-$counter  = count($users);
-$var = 1;
 
 
 
@@ -36,20 +38,11 @@ $var = 1;
 		<title>PHP TSI</title>
 	</head>
 	<body>
-		<?php if($second == false):?>
-		<form method="post" action="">
-			Start number:<br>
-			<input type="text" name="startNumber"/><br>
-			End number: <br>
-			<input type="text" name="endNumber"/><br>
-			Add by:<br>
-			<input type="text" name="addNumber"/><br>
-			<input type="submit" value="go!">
-			<?php endif;?>
-		<?php foreach($users['users'] as $key => $value): ?>
-		
-			<?php echo "Name " . $key . " Surname " . $value . " <br>";
-			$var++;?>
+		<?php  foreach($news_array as $options => $value): ?>
+			<?php if ($news_array[$options]['isActive'] == true): ?>
+			<h1><?php echo $news_array[$options]['news_tag']; ?></h1>
+			<p><?php echo " " . $news_array[$options]['news_content']; ?></p>
+		<?php endif; ?>
 		<?php endforeach; ?>
 	</body>
 </html>
